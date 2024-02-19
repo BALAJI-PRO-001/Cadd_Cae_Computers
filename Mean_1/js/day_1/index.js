7408e1ea5ce2b2b385d3cf85a6987c526a6edefc
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-02-19 19:44:55 +0530

    No commit message

--- Mean_1/js/day_1/index.js
+++ Mean_1/js/day_1/index.js
@@ -3,6 +3,8 @@
 console.log("Process Start ....");
 
 
+//================================================================
+
 // setTimeout(() => {
 //     console.log("cb is work ....");
 // }, 2000);
@@ -23,6 +25,22 @@ console.log("Process Start ....");
 // setTimeout(function (name) {
 //     console.log("Name : " + name);
 // }, 2000, "Balaji");
+
+
+//================================================================
+
+// setInterval((name) => {
+//     console.log("Name : " + name);
+// }, 2000, "Balaji");
+
+
+// let i = 1;
+// const id = setInterval(() => {              // 1 2 3 4 
+//     if (i == 5) clearInterval(id);
+//     console.log(i);
+//     i ++;
+// }, 2000);
+
 
 
 console.log("Process End .....");
