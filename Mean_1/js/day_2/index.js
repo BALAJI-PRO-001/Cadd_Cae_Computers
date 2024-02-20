9cd9777bebdeffb7a0dd502991decf2f1acc07e6
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-02-20 19:35:09 +0530

    No commit message

---
+++ Mean_1/js/day_2/index.js
@@ -0,0 +1,37 @@
+// console.log(window.history.length);
+// console.log(window.history.state);
+
+// history.back();
+// history.forward();
+// history.go(1);
+
+
+//============================================
+
+// console.log(navigator);
+// console.log(navigator.appCodeName);
+// console.log(navigator.appName);
+// console.log(navigator.appVersion);
+// console.log(navigator.cookieEnabled);
+// console.log(navigator.language);
+// console.log(navigator.onLine);
+// console.log(navigator.platform);
+// console.log(navigator.product);
+// console.log(navigator.userAgent);
+// console.log(navigator.javaEnabled());
+
+//============================================
+
+// console.log(location);
+// console.log(location.hostname);
+// console.log(location.host);
+// console.log(location.origin);
+// console.log(location.pathname);
+// console.log(location.href);
+// console.log(location.protocol);
+// console.log(location.search);
+
+
+// location.reload();
+// location.assign("http://caddcae.com/");
+// location.replace("http://caddcae.com/");
