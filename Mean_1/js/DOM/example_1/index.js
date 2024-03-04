1fe7fd942ebb5b081b9b719ceeebad05554c4e7c
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-03-04 19:33:03 +0530

    No commit message

---
+++ Mean_1/js/DOM/example_1/index.js
@@ -0,0 +1,37 @@
+// console.log(document.activeElement);
+// console.log(document.URL);
+// console.log(document.baseURI);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.title);
+// console.log(document.cookie);
+// console.log(document.domain);
+
+
+// ============================================================
+// console.log(document.forms);
+// console.log(document.forms[0]);
+// ============================================================
+
+
+// ============================================================
+// console.log(document.images);
+// console.log(document.images[0])
+// ============================================================
+
+
+// ============================================================
+// console.log(document.links);
+// console.log(document.links[0]);
+// ============================================================
+
+
+// ============================================================
+// console.log(document.doctype);
+// console.log(document.readyState);
+// console.log(document.scripts);
+// console.log(document.children);
+// console.log(document.childElementCount);
+// console.log(document.body.childElementCount);
+// console.log(document.currentScript);
+
