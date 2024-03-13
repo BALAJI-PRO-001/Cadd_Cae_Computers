8dd636bacf222107941820ca242ebeede872b44d
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2024-03-13 19:48:18 +0530

    No commit message

---
+++ Mean_1/js/DOM/example_5/index.js
@@ -0,0 +1,54 @@
+// const container = document.getElementById("container");
+// const parentElement = container.parentElement;
+
+// console.log("Child : ", container);
+// console.log("ParentElement Using Element : ",parentElement);
+// console.log("ParentElement Using Node : ", container.parentNode);
+
+// console.log("ParentElement => ParentElement : ",container.parentElement.parentElement);
+
+
+
+//==================================================================
+// const container = document.getElementById("container");
+// const childrens = container.children;
+// const childNodes = container.childNodes;
+
+// console.log("ParentElement : ", container);
+// console.log("Childrens : ", childrens);
+// console.log("ChildNodes : ", childNodes);
+// console.log("FirstChild Using Children : ", childrens[0]);
+// console.log("FirstChild Using ChildNodes : ", childNodes[0]);
+//==================================================================
+
+
+
+
+//==================================================================
+// const container = document.getElementById("container");
+// console.log("Child Elements Count : ", container.childElementCount);
+//==================================================================
+
+
+
+
+//==================================================================
+// const container = document.getElementById("container");
+// console.log(container.childNodes);
+
+// console.log("First Node Child", container.firstChild);
+// console.log("First Element Child", container.firstElementChild);
+// console.log("Last Node Child", container.lastChild);
+// console.log("Last Element Child", container.lastElementChild);
+//==================================================================
+
+
+
+//==================================================================
+// const container = document.getElementById("container");
+// console.log("CurrentElement : ", container.children[1]);
+// console.log("PreSiblingElement : ", container.children[1].previousElementSibling);
+// console.log("NextSiblingElement", container.children[1].nextElementSibling);
+// console.log("PreSiblingNode : ", container.children[1].previousSibling);
+// console.log("NextSiblingNode", container.children[1].nextSibling);
+//==================================================================
